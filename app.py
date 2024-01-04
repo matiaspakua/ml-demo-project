520c646724c0a60a9194703883b488d8258b3243
Matias Miguez <matiaspakua@gmail.com>
2024-01-04 14:19:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,6 @@ LOG.setLevel(logging.INFO)
 def home():
     html = f"<h3>Hello World Python Flask</h3>"
     return html.format(format)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
