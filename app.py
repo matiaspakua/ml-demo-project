35ba3b0c12b93356e4f3d241ddba8f6c547425b7
Matias Miguez <matiaspakua@gmail.com>
2024-01-04 14:19:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print(f"Hello world!")
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+
+import mlib
+
+app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+
+@app.route("/")
+def home():
+    html = f"<h3>Hello World Python Flask</h3>"
+    return html.format(format)
