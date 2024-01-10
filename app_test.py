4b5191663e84e4eb9e4a05f86b1f47ad7c50fa9a
Matias Miguez <matiaspakua@gmail.com>
2024-01-10 11:47:02 +0100

    Update app_test for prepare_image fuction

--- app_test.py
+++ app_test.py
@@ -37,3 +37,30 @@ def test_home_content(app):
 
         # Verify that the form contains the expected elements
         assert "<input type=" in content
+
+def test_prepare_image_with_valid_image_path():
+    # Set up
+    image_path = "image/image.jpg"
+
+    # Call the function
+    img_array = prepare_image(image_path)
+
+    # Check the shape of the image array
+    expected_shape = (1, 224, 224, 3)
+    assert img_array.shape == expected_shape
+
+    # Check the type of the image array
+    expected_type = np.float32
+    assert img_array.dtype == expected_type
+
+    # Check the values of the image array
+    assert 0.0 <= img_array.min() <= 1.0
+    assert img_array.max() == 1.0
+
+def test_prepare_image_with_invalid_image_path():
+    # Set up
+    invalid_image_path = "images/invalid_image_path.jpg"
+
+    # Call the function
+    with pytest.raises(FileNotFoundError):
+        img_array = prepare_image(invalid_image_path)
