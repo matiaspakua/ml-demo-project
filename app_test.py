a628644b1286ac0c10ce10179c1efa3efd52285d
Matias Miguez <matiaspakua@gmail.com>
2024-01-11 09:16:42 +0100

    Cleaning test

--- app_test.py
+++ app_test.py
@@ -11,11 +11,9 @@ from app import prepare_image
 @pytest.fixture
 def app():
     app = Flask(__name__)
-
     @app.route("/")
     def home():
         return render_template("view.html")
-
     return app
 
 
@@ -26,20 +24,15 @@ def test_home_endpoint(app):
         assert response.status_code == 200
         assert response.mimetype == "text/html"
         content = response.data.decode("utf-8")
-        # Check that the correct template is being used
-        # assert "view.html" in response.data.decode('utf-8')
+        # Check that the correct template is being used        
         assert content is not None
 
 
 def test_home_content(app):
     with app.test_client() as client:
         response = client.get("/")
-
         # Extract the HTML content
         content = response.data.decode("utf-8")
-
-        # print(f"content = {content}")
-
         # Verify that the form contains the expected elements
         assert "<input type=" in content
 
@@ -70,44 +63,3 @@ def test_prepare_image_with_invalid_image_path():
     with pytest.raises(FileNotFoundError):
         img_array = prepare_image(invalid_image_path)
 
-
-def test_predict_with_valid_image():
-    # Set up
-    valid_image_path = "images/test/image.jpg"
-    with open(valid_image_path, "rb") as f:
-        image = FileStorage(f)
-
-    # Mock the Flask request
-    mock_request = Flask.Request.from_values(files={"image": image})
-
-    # Call the function
-    with app.test_request_context(method="POST", data=mock_request.data):
-        response = app.test_client().get("/predict")
-
-    # Parse the response JSON
-    response_data = json.loads(response.data.decode())
-
-    # Check the response
-    assert response_data["response"] == "OK"
-    assert response_data["predictions"]
-    assert len(response_data["predictions"]) == 3
-    assert isinstance(response_data["predictions"][0], dict)
-    assert "label" in response_data["predictions"][0]
-    assert "probability" in response_data["predictions"][0]
-
-def test_predict_with_invalid_image():
-    # Set up
-    invalid_image_path = "images/test/invalid_image.png"
-    with open(invalid_image_path, "rb") as f:
-        image = FileStorage(f)
-
-    # Mock the Flask request
-    mock_request = flask.Request.from_values(files={"image": image})
-
-    # Call the function
-    with app.test_request_context(method="POST", data=mock_request.data):
-        response = app.test_client().get("/predict")
-
-    # Check the response
-    assert response.status_code == 400
-    assert response.data == b"Invalid image format"
