4a5aeec6039799795323af96180db71386a1f5a8
Matias Miguez <matiaspakua@gmail.com>
2024-01-11 09:12:33 +0100

    Code comments.

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
+# This line creates an instance of the MobileNetV2 neural network model from Keras 
+# with pre-trained weights on the ImageNet dataset.
 model = MobileNetV2(weights="imagenet")
 
 
@@ -18,7 +20,11 @@ model = MobileNetV2(weights="imagenet")
 def home():
     return render_template("view.html")
 
-
+# Preprocessing an image before feeding it into a neural network, specifically a MobileNetV2 model. It 
+# resizes the image to a square shape with dimensions of 224x224 pixels. MobileNetV2, expect input 
+# images of a specific size.
+# The deep learning framework Keras preprocesses the image array to be compatible with the MobileNetV2 
+# model. It may involve normalization and other transformations.
 def prepare_image(image):
     img = Image.open(image)
     img = img.resize((224, 224))
@@ -28,6 +34,7 @@ def prepare_image(image):
     return img_array
 
 
+# The model return a list of the top 3 posible results with highest probability.
 @app.route("/predict", methods=["POST"])
 def predict():
     image = request.files["image"]
@@ -35,16 +42,11 @@ def predict():
     img_array = prepare_image(image)
     LOG.info(f"Calling to model...")
     prediction = model.predict(img_array)
-    results = tf.keras.applications.mobilenet_v2.decode_predictions(prediction, top=3)[
-        0
-    ]
-    response = []
+    results = tf.keras.applications.mobilenet_v2.decode_predictions(prediction, top=3)[0]
+    response = []    
     for result in results:
         response.append({"label": result[1], "probability": float(result[2])})
-
-    return render_template("result.html", response=response)
-    # return jsonify(response)
-
+    return render_template("result.html", response=response)    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
